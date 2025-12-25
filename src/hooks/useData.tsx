bb2d0f8a449a84862e569d31502ad43497ab834b
Luis Medrano <ing.luismedrano0723@gmail.com>
2025-12-25 10:55:04 -0400

    newPro

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export const useData = () => {
 
-    const images: string[] = [`img/Screenshot 2025-12-21 175547.png`, 'img/Screenshot 2025-12-21 175601.png', 'img/Screenshot 2025-12-21 175615.png', 'img/Screenshot 2024-05-26 211211.png'];
-    const projectsTitles: string[] = ['Task List', 'Gift Search', 'Movies Search', 'Sport Platform']
+    const images: string[] = [`img/Screenshot 2025-12-21 175547.png`, 'img/Screenshot 2025-12-21 175601.png', 'img/Screenshot 2025-12-21 175615.png', 'img/Screenshot 2024-05-26 211211.png','img/Screenshot 2025-12-25 104842.png'];
+    const projectsTitles: string[] = ['Task List', 'Gift Search', 'Movies Search', 'Sport Platform','Scrumble Words Game']
     const skills: string[] = ["React", "TypeScript", "UI/UX Design", "Animation", 'C#', 'Database', 'Tailwind', 'Git', 'Azure']
-    const gitHubLinks: string[] = ["", "", "https://github.com/Ing-MedranoLuis/MoviesAPI", 'https://github.com/Ing-MedranoLuis/Sportplatform']
-     const viewLinks: string[] = ["https://taskingmedrano.netlify.app/", "https://preeminent-melba-8c4048.netlify.app/", "https://moviesapiconsult.netlify.app/", '' ]
+    const gitHubLinks: string[] = ["", "", "https://github.com/Ing-MedranoLuis/MoviesAPI", 'https://github.com/Ing-MedranoLuis/Sportplatform','https://github.com/Ing-MedranoLuis/ScrumbleWordsGame']
+     const viewLinks: string[] = ["https://taskingmedrano.netlify.app/", "https://preeminent-melba-8c4048.netlify.app/", "https://moviesapiconsult.netlify.app/", '','https://scrumblewordgames.netlify.app/' ]
         const socialMedias: string[] = ['https://www.linkedin.com/feed/', 'https://github.com/Ing-MedranoLuis?tab=repositories', 'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox']
    
     const detailsProjects: string[] = [
